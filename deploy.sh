d6c736c558a0ae67727d7af7f794094583ff3f23
Matthew Oberlander <moberlander@econsys.com>
2019-06-25 20:09:50 -0400

    Fixed taging on images

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #buld the docker images 
 #(docker image build -t tag_name -f path/dockerfile ./build_context)
 docker image build -t moberlander08/multi-client:latest -t moberlander08/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker image build -t moberlander08/multi-server:latest -t moberlander08/multi-client:$GIT_SHA -f ./server/Dockerfile ./server
-docker image build -t moberlander08/multi-worker:latest -t moberlander08/multi-client:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker image build -t moberlander08/multi-server:latest -t moberlander08/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker image build -t moberlander08/multi-worker:latest -t moberlander08/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
 #Push images to docker hub
 docker push moberlander08/multi-client:latest
